1b7c38fad21851e2e7a185e124921b85e7e5e25c
jhinariv <jhina.riv@gmail.com>
2022-11-06 16:33:39 -0500

    Ajuste contantes

--- app/forms.py
+++ app/forms.py
@@ -10,6 +10,8 @@ from flask_wtf.file     import FileField, FileRequired,FileAllowed
 from wtforms            import StringField, TextAreaField, SubmitField, PasswordField,DateTimeField,DecimalField
 from wtforms.validators import InputRequired, Email, DataRequired, Regexp, EqualTo
 
+DESC_FILE_FIELD = "Solo archivos de voz!"
+
 class LoginForm(FlaskForm):
 	username    = StringField  (u'Username', validators=[DataRequired()])
 	password    = PasswordField(u'Password', validators=[DataRequired()])
@@ -24,7 +26,7 @@ class RegisterForm(FlaskForm):
 class createConcursoForm(FlaskForm):
 	name        = StringField  (u'Nombre', validators=[DataRequired()])
 	url_concurso    = StringField  (u'URL para el concurso')
-	url_imagen = FileField(u'Imagen', validators=[FileAllowed(['mp3', 'ogg','wav'], 'Solo archivos de voz!')])
+	url_imagen = FileField(u'Imagen', validators=[FileAllowed(['mp3', 'ogg','wav'], DESC_FILE_FIELD)])
 	fecha_inicio = DateTimeField(u'Fecha Inicio',id="fecha_inicio" ,format='%Y/%m/%d', validators=[DataRequired()])
 	fecha_fin = DateTimeField(u'Fecha Fin', format='%Y/%m/%d',validators=[DataRequired()])
 	valor_pago = DecimalField(u'Valor a pagar', validators=[DataRequired()])
@@ -34,7 +36,7 @@ class createConcursoForm(FlaskForm):
 class updateConcursoForm(FlaskForm):
 	name        = StringField  (u'Nombre', validators=[DataRequired()])
 	url_concurso    = StringField  (u'URL para el concurso')
-	url_imagen = FileField(u'Imagen', validators=[FileAllowed(['mp3', 'ogg','wav'], 'Solo archivos de voz!')])
+	url_imagen = FileField(u'Imagen', validators=[FileAllowed(['mp3', 'ogg','wav'], DESC_FILE_FIELD)])
 	fecha_inicio = DateTimeField(u'Fecha Inicio',id="fecha_inicio" ,format='%Y/%m/%d', validators=[DataRequired()])
 	fecha_fin = DateTimeField(u'Fecha Fin', format='%Y/%m/%d',validators=[DataRequired()])
 	valor_pago = DecimalField(u'Valor a pagar', validators=[DataRequired()])
@@ -45,5 +47,5 @@ class createVozForm(FlaskForm):
 	name        = StringField  (u'name', validators=[DataRequired()])
 	lastname    = StringField  (u'lastname', validators=[DataRequired()])
 	email       = StringField  (u'email' , validators=[DataRequired(), Email(message='El correo no tiene el formato adecuado')])
-	profile = FileField(u'profile', validators=[FileRequired(), FileAllowed(['mp3', 'ogg','wav'], 'Solo archivos de voz!')])
+	profile = FileField(u'profile', validators=[FileRequired(), FileAllowed(['mp3', 'ogg','wav'], DESC_FILE_FIELD)])
 	observaciones = TextAreaField(u'Observaciones')
