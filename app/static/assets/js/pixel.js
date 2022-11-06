a9a73ad5ada82b206ce9f4bc6300dfb3bb79e4d6
afbenitez <47255134+afbenitez@users.noreply.github.com>
2022-11-06 18:16:35 -0500

    Update pixel.js

--- app/static/assets/js/pixel.js
+++ app/static/assets/js/pixel.js
@@ -32,8 +32,6 @@ d.addEventListener("DOMContentLoaded", function(event) {
 
         const animations = ['oneByOne', 'delayed', 'sync', 'scenario'];
 
-        new Vivus('loader-logo', {duration: 80, type: 'oneByOne'}, function () {});
-
         setTimeout(function() {
             preloader.classList.add('show');
         }, 1500);
